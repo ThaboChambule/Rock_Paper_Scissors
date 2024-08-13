10f58ad7d37c99ed376a240bd6cb4db8173b8e6b
thabo <thabochambule1@gmail.com>
2024-08-13 11:17:29 +0200

    Build a computer move function

--- script/script.js
+++ script/script.js
@@ -2,3 +2,18 @@ const buttonElement = document.querySelector(".js-rock");
 buttonElement.addEventListener("click", () => {
   console.log("Testing");
 });
+
+const computerMove = () => {
+  const moveComputer = Math.random();
+  let result = "";
+
+  if (moveComputer >= 0 && moveComputer >= 1 / 3) {
+    result = "Rock";
+  } else if (moveComputer >= 1 / 3 && moveComputer >= 2 / 3) {
+    result = "Paper";
+  } else if (moveComputer >= 2 / 3 && moveComputer >= 1) result = "Scissors";
+
+  return result;
+
+};
+
